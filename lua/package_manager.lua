4030404c9d497e2245667b3f8627fc91fd9fd53d
Ivan Duran <idurandiaz@gmail.com>
2025-07-30 23:59:01 +0300

    refactor: add package manager logic

---
+++ lua/package_manager.lua
@@ -0,0 +1,297 @@
+local M = {}
+
+M.plugins = {}
+M.loaded_plugins = {}
+
+-- Plugin specification
+function M.add(spec)
+  -- spec = {
+  --   src = "https://github.com/user/plugin", -- git URL
+  --   name = "plugin", -- optional, derived from src if not provided
+  --   enabled = true, -- default true, set false to disable
+  --   version = "v1.2.3", -- git tag, branch, or commit hash
+  --   build = "make", -- string command or function
+  --   event = "InsertEnter", -- or table of events
+  --   ft = "lua", -- or table of filetypes
+  --   keys = "<leader>ff", -- string, array of strings, or LazyVim format
+  --   cmd = "CommandName", -- or table of commands
+  --   config = function() end, -- setup function (runs only once)
+  --   lazy = true -- default true
+  -- }
+
+  local name = spec.name or spec.src:match('([^/]+)$'):gsub('%.git$', '')
+  spec.name = name
+  spec.enabled = spec.enabled ~= false -- default to true if not specified
+
+  M.plugins[name] = spec
+
+  -- Only setup if enabled
+  if not spec.enabled then return end
+
+  -- If not lazy, load immediately
+  if spec.lazy == false then
+    M.load_plugin(spec)
+    return
+  end
+
+  -- Setup lazy loading
+  M.setup_lazy_loading(spec)
+end
+
+function M.load_plugin(spec)
+  -- Only load and configure once
+  if M.loaded_plugins[spec.name] then
+    return -- Already loaded
+  end
+
+  local pack_spec = {
+    src = spec.src,
+    name = spec.name,
+    version = spec.version,
+  }
+  vim.pack.add({ pack_spec })
+
+  -- Mark as loaded first to prevent recursion
+  M.loaded_plugins[spec.name] = true
+
+  -- Run config only on first load
+  if spec.config then spec.config() end
+end
+
+function M.setup_lazy_loading(spec)
+  local group = vim.api.nvim_create_augroup('simple-pack-' .. spec.name, { clear = true })
+
+  -- Event-based lazy loading
+  if spec.event then M.lazyload_on_events(spec, group) end
+
+  -- FileType-based lazy loading
+  if spec.ft then M.lazyload_on_filetypes(spec, group) end
+
+  -- Enhanced keymap-based lazy loading
+  if spec.keys then M.lazyload_on_keys(spec) end
+
+  -- Command-based lazy loading
+  if spec.cmd then
+    local commands = type(spec.cmd) == 'table' and spec.cmd or { spec.cmd }
+    for _, cmd in ipairs(commands) do
+      vim.api.nvim_create_autocmd('CmdUndefined', {
+        group = group,
+        pattern = cmd,
+        callback = function()
+          M.load_plugin(spec)
+          vim.api.nvim_exec_autocmds('CmdUndefined', { pattern = cmd })
+        end,
+        once = true,
+      })
+    end
+  end
+end
+
+-- Lazy load on keymaps
+function M.lazyload_on_keys(spec)
+  local keys = type(spec.keys) == 'table' and spec.keys or { spec.keys }
+
+  for _, key in ipairs(keys) do
+    if type(key) == 'string' then
+      -- Simple string format: "keys = '<leader>ff'"
+      vim.keymap.set('n', key, function()
+        M.load_plugin(spec)
+        -- Re-trigger the keymap after loading
+        vim.api.nvim_feedkeys(key, 'n', false)
+      end, { desc = '[Lazy] Load ' .. spec.name })
+    elseif type(key) == 'table' then
+      -- Advanced format: { modes, lhs, rhs, opts }
+      local modes = key[1] or 'n'
+      local lhs = key[2]
+      local rhs = key[3]
+      local opts = key[4] or {}
+
+      -- Create one-shot lazy loading wrapper
+      local lazy_rhs = function()
+        M.load_plugin(spec)
+        if type(rhs) == 'string' then
+          -- Parse string commands
+          if rhs:match('^<cmd>.*<cr>$') then
+            local cmd = rhs:match('^<cmd>(.+)<cr>$')
+            vim.cmd(cmd)
+          elseif rhs:match('^:.*<cr>?$') then
+            local cmd = rhs:match('^:(.+)<cr>?$')
+            vim.cmd(cmd)
+          else
+            vim.api.nvim_feedkeys(rhs, 'n', false)
+          end
+        elseif type(rhs) == 'function' then
+          rhs()
+        end
+      end
+
+      vim.keymap.set(modes, lhs, lazy_rhs, opts)
+    end
+  end
+end
+
+-- Lazy load on events
+function M.lazyload_on_events(spec, group)
+  local events = type(spec.event) == 'table' and spec.event or { spec.event }
+  vim.api.nvim_create_autocmd(events, {
+    group = group,
+    callback = function() M.load_plugin(spec) end,
+    once = true,
+  })
+end
+
+-- Lazy load on filetypes
+function M.lazyload_on_filetypes(spec, group)
+  local filetypes = type(spec.ft) == 'table' and spec.ft or { spec.ft }
+  vim.api.nvim_create_autocmd('FileType', {
+    group = group,
+    pattern = filetypes,
+    callback = function() M.load_plugin(spec) end,
+    once = true,
+  })
+end
+
+-- Get enabled plugin names
+function M.get_enabled_plugin_names()
+  local enabled = {}
+  for plugin_name, spec in pairs(M.plugins) do
+    if spec.enabled then table.insert(enabled, plugin_name) end
+  end
+  return enabled
+end
+
+-- Install missing plugins (only enabled ones)
+function M.install()
+  print('Installing plugins...')
+
+  local enabled_specs = {}
+  for _, spec in pairs(M.plugins) do
+    if spec.enabled then
+      table.insert(enabled_specs, {
+        src = spec.src,
+        name = spec.name,
+        version = spec.version,
+      })
+    end
+  end
+
+  if #enabled_specs > 0 then
+    vim.pack.add({ enabled_specs })
+
+    -- Run build commands after installation
+    for _, spec in pairs(M.plugins) do
+      if spec.enabled and spec.build then M.run_build(spec) end
+    end
+  end
+
+  print('Installation completed!')
+end
+
+-- Update enabled plugins using vim.pack.update()
+function M.update(opts)
+  opts = opts or {}
+  local force = opts.force or false
+
+  local enabled_names = M.get_enabled_plugin_names()
+
+  if #enabled_names == 0 then
+    print('No enabled plugins to update')
+    return
+  end
+
+  print('Updating plugins...')
+
+  -- Use vim.pack.update with the new API
+  vim.pack.update(enabled_names, { force = force })
+
+  -- Run build commands after update
+  vim.schedule(function()
+    for _, spec in pairs(M.plugins) do
+      if spec.enabled and spec.build then M.run_build(spec) end
+    end
+    print('Update completed!')
+  end)
+end
+
+-- Remove disabled plugins using vim.pack.del()
+function M.clean()
+  local pack_list = vim.pack.list()
+  local to_delete = {}
+
+  for _, info in ipairs(pack_list) do
+    local name = info.spec.name
+    local spec = M.plugins[name]
+    if not spec or not spec.enabled then
+      table.insert(to_delete, name)
+      print('Marking ' .. name .. ' for removal...')
+    end
+  end
+
+  if #to_delete > 0 then
+    vim.pack.del(to_delete)
+    print('Cleanup completed!')
+  else
+    print('No plugins to clean')
+  end
+end
+
+-- Sync: Install enabled, update enabled, remove disabled
+function M.sync(opts)
+  opts = opts or {}
+
+  print('Starting sync...')
+
+  -- Install missing enabled plugins
+  M.install()
+
+  -- Update existing enabled plugins
+  M.update({ force = opts.force })
+
+  -- Remove disabled plugins
+  M.clean()
+
+  print('Sync completed!')
+end
+
+-- Get plugin info using vim.pack.list()
+function M.status()
+  local pack_list = vim.pack.list()
+  local installed_map = {}
+
+  for _, info in ipairs(pack_list) do
+    installed_map[info.spec.name] = info
+  end
+
+  print('Plugin Status:')
+  print('==============')
+
+  for name, spec in pairs(M.plugins) do
+    local status_icon = spec.enabled and '✓' or '✗'
+    local loaded_icon = M.loaded_plugins[name] and ' [L]' or ''
+    local info = installed_map[name]
+
+    if info then
+      local version_info = info.spec.version or 'default branch'
+      local active_status = info.active and '(loaded)' or '(not loaded)'
+      print(string.format('%s %s%s: %s %s', status_icon, name, loaded_icon, version_info, active_status))
+    else
+      local install_status = spec.enabled and 'not installed' or 'disabled'
+      print(string.format('%s %s%s: %s', status_icon, name, loaded_icon, install_status))
+    end
+  end
+
+  -- Show orphaned plugins (installed but not in config)
+  local orphans = {}
+  for _, info in ipairs(pack_list) do
+    if not M.plugins[info.spec.name] then table.insert(orphans, info.spec.name) end
+  end
+
+  if #orphans > 0 then
+    print('\nOrphaned plugins (not in config):')
+    for _, plugin in ipairs(orphans) do
+      print('⚠ ' .. plugin)
+    end
+  end
+end
+
+return M
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,27 +1,32 @@
 -- vim: foldmethod=marker foldlevel=0
 
+local manager = require('package_manager')
+local p = manager.add
+
 --: Lazy load treesitter {{{
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWritePre" }, {
-  group = vim.api.nvim_create_augroup("idr4n/lazy/treesitter", { clear = true }),
-  callback = function ()
-    vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
+p({
+  src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+  event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+  config = function()
+    vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' })
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "go", "markdown", "markdown_inline" },
+      ensure_installed = { 'lua', 'vim', 'vimdoc', 'query', 'go', 'markdown', 'markdown_inline' },
       highlight = { enable = true, additional_vim_regex_highlighting = false },
       indent = { enable = true },
     })
-  end
+  end,
 })
 --: }}}
 
 --: Lazy load mini.diff {{{
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWritePre" }, {
-  group = vim.api.nvim_create_augroup("idr4n/lazy/minidiff", { clear = true }),
-  callback = function ()
-    vim.pack.add({ "https://github.com/echasnovski/mini.diff" })
+p({
+  src = 'https://github.com/echasnovski/mini.diff',
+  event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+  config = function()
+    vim.pack.add({ 'https://github.com/echasnovski/mini.diff' })
     local minidiff = require('mini.diff')
     minidiff.setup({
-      view = { signs = { add = ' +', change = ' ~', delete = ' -' }, },
+      view = { signs = { add = ' +', change = ' ~', delete = ' -' } },
       mappings = {
         apply = '<leader>hs',
         reset = '<leader>hS',
@@ -32,44 +37,46 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWritePre" }, {
         goto_last = ']C',
       },
     })
-    vim.keymap.set("n", "<leader>gt", minidiff.toggle, { desc = "Toggle Mini Diff" })
-    vim.keymap.set("n", "<leader>go", minidiff.toggle_overlay, { desc = "Toggle Mini Diff" })
-  end
+    vim.keymap.set('n', '<leader>gt', minidiff.toggle, { desc = 'Toggle Mini Diff' })
+    vim.keymap.set('n', '<leader>go', minidiff.toggle_overlay, { desc = 'Toggle Mini Diff' })
+  end,
 })
 --: }}}
 
 --: Lazy load mini.surround {{{
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWritePre" }, {
-  group = vim.api.nvim_create_augroup("idr4n/lazy/minisurround", { clear = true }),
-  callback = function ()
-    vim.pack.add({ "https://github.com/echasnovski/mini.surround" })
-    require("mini.surround").setup({
+p({
+  src = 'https://github.com/echasnovski/mini.surround',
+  event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+  config = function()
+    vim.pack.add({ 'https://github.com/echasnovski/mini.surround' })
+    require('mini.surround').setup({
       mappings = {
-        add = "S",
-        delete = "ds",
-        replace = "cs",
-        find = "fsr",
-        find_left = "fsl",
-        highlight = "",
-        update_n_lines = "",
+        add = 'S',
+        delete = 'ds',
+        replace = 'cs',
+        find = 'fsr',
+        find_left = 'fsl',
+        highlight = '',
+        update_n_lines = '',
       },
     })
-  end
+  end,
 })
 --: }}}
 
 --: Lazy load nvim-autopairs {{{
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-  group = vim.api.nvim_create_augroup("idr4n/lazy/nvimautopairs", { clear = true }),
-  callback = function ()
-    vim.pack.add({ "https://github.com/windwp/nvim-autopairs" })
-    local npairs = require("nvim-autopairs")
-    local Rule = require("nvim-autopairs.rule")
-    local cond = require("nvim-autopairs.conds")
+p({
+  src = 'https://github.com/windwp/nvim-autopairs',
+  event = { 'BufReadPost', 'BufNewFile' },
+  config = function()
+    vim.pack.add({ 'https://github.com/windwp/nvim-autopairs' })
+    local npairs = require('nvim-autopairs')
+    local Rule = require('nvim-autopairs.rule')
+    local cond = require('nvim-autopairs.conds')
     npairs.setup()
-    local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+    local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
     npairs.add_rules({
-      Rule(" ", " ")
+      Rule(' ', ' ')
         :with_pair(function(opts)
           local pair = opts.line:sub(opts.col - 1, opts.col)
           return vim.tbl_contains({
@@ -84,36 +91,114 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
           local col = vim.api.nvim_win_get_cursor(0)[2]
           local context = opts.line:sub(col - 1, col + 2)
           return vim.tbl_contains({
-            brackets[1][1] .. "  " .. brackets[1][2],
-            brackets[2][1] .. "  " .. brackets[2][2],
-            brackets[3][1] .. "  " .. brackets[3][2],
+            brackets[1][1] .. '  ' .. brackets[1][2],
+            brackets[2][1] .. '  ' .. brackets[2][2],
+            brackets[3][1] .. '  ' .. brackets[3][2],
           }, context)
         end),
     })
     for _, bracket in pairs(brackets) do
       npairs.add_rules({
-        Rule(bracket[1] .. " ", " " .. bracket[2])
+        Rule(bracket[1] .. ' ', ' ' .. bracket[2])
           :with_pair(cond.none())
-          :with_move(function(opts)
-            return opts.char == bracket[2]
-          end)
+          :with_move(function(opts) return opts.char == bracket[2] end)
           :with_del(cond.none())
           :use_key(bracket[2])
-          :replace_map_cr(function(_)
-            return "<C-c>2xi<CR><C-c>O"
-          end),
+          :replace_map_cr(function(_) return '<C-c>2xi<CR><C-c>O' end),
       })
     end
-  end
+  end,
+})
+--: }}}
+
+--: Lazy load fzf-lua {{{
+p({
+  src = 'https://github.com/ibhagwan/fzf-lua',
+  config = function()
+    require('fzf-lua').setup({
+      winopts = {
+        -- Open in a split at the bottom instead of floating window
+        -- split = "belowright 13new",
+        row = 0.5,
+        height = 0.7,
+        width = 89,
+        backdrop = 100,
+        preview = {
+          hidden = true,
+          layout = 'vertical',
+          -- layout = "horizontal",
+          vertical = 'down:50%',
+          horizontal = 'right:50%',
+        },
+      },
+      keymap = {
+        builtin = {
+          ['<C-l>'] = 'toggle-preview',
+          ['<C-d>'] = 'preview-page-down',
+          ['<C-u>'] = 'preview-page-up',
+        },
+        fzf = {
+          ['ctrl-l'] = 'toggle-preview',
+          ['ctrl-q'] = 'select-all+accept',
+        },
+      },
+    })
+  end,
+  keys = {
+    { 'n', '<C-Space>', '<cmd>FzfLua files<cr>', { desc = 'FzfLua Files' } },
+    { 'n', '<leader>sk', '<cmd>FzfLua keymaps<cr>', { desc = 'FzfLua Keymaps' } },
+  },
+})
+--: }}}
+
+--: Lazy load nvim-highlight-colors {{{
+p({
+  src = 'https://github.com/brenoprata10/nvim-highlight-colors',
+  ft = { 'cfg', 'css', 'html', 'conf', 'lua', 'scss', 'toml', 'markdown', 'typescript', 'typescriptreact' },
+  config = function()
+    vim.pack.add({ 'https://github.com/brenoprata10/nvim-highlight-colors' })
+    require('nvim-highlight-colors').setup({
+      render = 'virtual',
+      virtual_symbol = '󱓻',
+      exclude_filetypes = {},
+      exclude_buftypes = {},
+    })
+    vim.keymap.set('n', ',c', '<cmd>HighlightColors Toggle<cr>', { silent = true, desc = 'Toggle colorizer' })
+  end,
+})
+--: }}}
+
+--: Lazy load netrw-preview {{{
+p({
+  src = 'https://github.com/idr4n/netrw-preview.nvim',
+  config = function()
+    require('netrw-preview').setup({
+      preview_width = 65,
+      mappings = {
+        close_netrw = { 'q', 'gq', '<c-q>' },
+        toggle_preview = { 'p', '<Tab>' },
+        directory_mappings = {
+          { key = '~', path = '~', desc = 'Home directory' },
+          { key = 'gd', path = '~/Downloads', desc = 'Downloads directory' },
+          { key = 'gw', path = function() return vim.fn.getcwd() end, desc = 'Current working directory' },
+        },
+      },
+    })
+  end,
+  keys = {
+    { 'n', ',,', '<cmd>NetrwRevealToggle<cr>', { desc = 'Toggle Netrw - Reveal' } },
+    { 'n', ',l', '<cmd>NetrwRevealLexToggle<cr>', { desc = 'Toggle Netrw (Lex) - Reveal' } },
+  },
 })
 --: }}}
 
 --: Lazy load blink-cmp {{{
-vim.api.nvim_create_autocmd("InsertEnter", {
-  group = vim.api.nvim_create_augroup("idr4n/lazy/blink.cmp", { clear = true }),
-  callback = function()
-    vim.pack.add({ { src = 'https://github.com/saghen/blink.cmp', version = 'v1.6.0' } })
-    require('blink.cmp').setup {
+p({
+  src = 'https://github.com/saghen/blink.cmp',
+  event = 'InsertEnter',
+  version = 'v1.6.0',
+  config = function()
+    require('blink.cmp').setup({
       cmdline = {
         enabled = false,
         completion = {
@@ -123,10 +208,10 @@ vim.api.nvim_create_autocmd("InsertEnter", {
       },
 
       sources = {
-        default = { "lsp", "path", "buffer", "snippets", },
+        default = { 'lsp', 'path', 'buffer', 'snippets' },
         providers = {
-          lsp = { opts = { tailwind_color_icon = '󱓻' }, }
-        }
+          lsp = { opts = { tailwind_color_icon = '󱓻' } },
+        },
       },
 
       completion = {
@@ -135,7 +220,7 @@ vim.api.nvim_create_autocmd("InsertEnter", {
         menu = {
           auto_show = true,
           draw = {
-            gap =  2,
+            gap = 2,
           },
         },
         documentation = {
@@ -145,124 +230,22 @@ vim.api.nvim_create_autocmd("InsertEnter", {
       },
 
       keymap = {
-        preset = "enter",
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<C-y>"] = { "select_and_accept" },
-        ["<C-j>"] = { "snippet_forward", "fallback" },
-        ["<C-k>"] = { "snippet_backward", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback" },
-        ["<C-n>"] = { "select_next", "fallback" },
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        preset = 'enter',
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<C-y>'] = { 'select_and_accept' },
+        ['<C-j>'] = { 'snippet_forward', 'fallback' },
+        ['<C-k>'] = { 'snippet_backward', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
       },
-    }
-  end,
-  once = true,
-})
---: }}}
-
---: Lazy load fzf-lua {{{
-vim.g.fzf_lua_loaded = false
-local fzf_lua_config = function()
-  require("fzf-lua").setup({
-    winopts = {
-      -- Open in a split at the bottom instead of floating window
-      -- split = "belowright 13new",
-      row = 0.5,
-      height = 0.7,
-      width = 89,
-      backdrop = 100,
-      preview = {
-        hidden = true,
-        layout = "vertical",
-        -- layout = "horizontal",
-        vertical = "down:50%",
-        horizontal = "right:50%",
-      },
-    },
-    keymap = {
-      builtin = {
-        ["<C-l>"] = "toggle-preview",
-        ["<C-d>"] = "preview-page-down",
-        ["<C-u>"] = "preview-page-up",
-      },
-      fzf = {
-        ["ctrl-l"] = "toggle-preview",
-        ["ctrl-q"] = "select-all+accept",
-      },
-    },
-  })
-end
-vim.keymap.set("n", "<C-Space>", function()
-  if not vim.g.fzf_lua_loaded then
-    vim.pack.add({ "https://github.com/ibhagwan/fzf-lua" })
-    fzf_lua_config()
-    vim.g.fzf_lua_loaded = true
-  end
-  vim.cmd("FzfLua files")
-end, { desc = "FzfLua Files" })
-vim.keymap.set("n", "<leader>sk", function()
-  if not vim.g.fzf_lua_loaded then
-    vim.pack.add({ "https://github.com/ibhagwan/fzf-lua" })
-    fzf_lua_config()
-    vim.g.fzf_lua_loaded = true
-  end
-  vim.cmd("FzfLua keymaps")
-end, { desc = "FzfLua Keymaps" })
---: }}}
-
---: Lazy load nvim-highlight-colors {{{
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("idr4n/lazy/nvim-highlight-colors", { clear = true }),
-  pattern = { "cfg", "css", "html", "conf", "lua", "scss", "toml", "markdown", "typescript", "typescriptreact", },
-  callback = function ()
-    vim.pack.add({ "https://github.com/brenoprata10/nvim-highlight-colors" })
-    require("nvim-highlight-colors").setup({
-      render = "virtual",
-      virtual_symbol = "󱓻",
-      exclude_filetypes = {},
-      exclude_buftypes = {},
     })
-    vim.keymap.set("n", ",c", "<cmd>HighlightColors Toggle<cr>", { silent = true, desc = "Toggle colorizer" })
-  end
+  end,
 })
---: }}}
-
---: Lazy load netrw-preview {{{
-vim.g.netrwpreview_loaded = false
-local netrwpreview_config = function()
-  require('netrw-preview').setup({
-    preview_width = 65,
-    mappings = {
-      close_netrw = { "q", "gq", "<c-q>" },
-      toggle_preview = { "p", "<Tab>" },
-      directory_mappings = {
-        { key = "~", path = "~", desc = "Home directory" },
-        { key = "gd", path = "~/Downloads", desc = "Downloads directory" },
-        { key = "gw", path = function() return vim.fn.getcwd() end, desc = "Current working directory", },
-      },
-    },
-  })
-end
-vim.keymap.set("n", ",,", function ()
-  if not vim.g.netrwpreview_loaded then
-    vim.pack.add({ "https://github.com/idr4n/netrw-preview.nvim" })
-    netrwpreview_config()
-    vim.g.netrwpreview_loaded = true
-  end
-  vim.cmd("NetrwRevealToggle")
-end, { desc = "Toggle Netrw - Reveal" })
-vim.keymap.set("n", ",l", function ()
-  if not vim.g.netrwpreview_loaded then
-    vim.pack.add({ "https://github.com/idr4n/netrw-preview.nvim" })
-    netrwpreview_config()
-    vim.g.netrwpreview_loaded = true
-  end
-  vim.cmd("NetrwRevealLexToggle")
-end, { desc = "Toggle Netrw (Lex) - Reveal" })
 --: }}}
