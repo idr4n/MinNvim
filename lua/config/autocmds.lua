fa97b7484712bec501b62500ba427c262d569930
Ivan Duran <idurandiaz@gmail.com>
2025-08-04 12:39:18 +0300

    refactor: update FileChangedShellPost autocmd
    - Wrap reload command in vim.schedule
    - Also apply stylua formatting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,97 +1,90 @@
 -- Autocommands
 local aucmd = vim.api.nvim_create_autocmd
-local function augroup(name)
-  return vim.api.nvim_create_augroup("idr4n/" .. name, { clear = true })
-end
+local function augroup(name) return vim.api.nvim_create_augroup('idr4n/' .. name, { clear = true }) end
 
 -- Custom message without loading time
-local startup_group = augroup("StartUpScreen")
-vim.api.nvim_create_autocmd("VimEnter", {
+local startup_group = augroup('StartUpScreen')
+vim.api.nvim_create_autocmd('VimEnter', {
   group = startup_group,
-  desc = "Show minimal startup screen",
+  desc = 'Show minimal startup screen',
   callback = function()
     -- require('utils').show_startup_screen("", false)
-    require('utils').show_startup_screen("Neovim")
+    require('utils').show_startup_screen('Neovim')
     -- require('utils').show_startup_screen("Neovim", false)
   end,
 })
 
 -- Restore normal settings when opening actual files
-aucmd({"BufRead", "BufNewFile"}, {
+aucmd({ 'BufRead', 'BufNewFile' }, {
   group = startup_group,
-  callback = function()
-    require('utils').restore_ui_settings()
-  end,
+  callback = function() require('utils').restore_ui_settings() end,
 })
 
 -- close some filetypes with <q>
-aucmd("FileType", {
-  group = augroup("close_with_q"),
+aucmd('FileType', {
+  group = augroup('close_with_q'),
   pattern = {
-    "help",
-    "lspinfo",
-    "man",
-    "qf",
-    "query",
-    "checkhealth",
+    'help',
+    'lspinfo',
+    'man',
+    'qf',
+    'query',
+    'checkhealth',
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
   end,
 })
 
 -- go to last loc when opening a buffer
-aucmd("BufReadPost", {
-  group = augroup("LastLocation"),
+aucmd('BufReadPost', {
+  group = augroup('LastLocation'),
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
+    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
   end,
 })
 
 -- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
+vim.api.nvim_create_autocmd('TextYankPost', {
   group = augroup('HighlightOnYank'),
-  callback = function()
-    vim.highlight.on_yank({ timeout = 70 })
-  end,
+  callback = function() vim.highlight.on_yank({ timeout = 70 }) end,
 })
 
 -- Lazy load statusline
 aucmd({ 'BufReadPost', 'BufNewFile' }, {
   group = augroup('LazyLoadStatusline'),
   once = true,
-  callback = function()
-    vim.opt.statusline = '%!v:lua.require("config.statusline").StatusLine()'
-  end,
+  callback = function() vim.opt.statusline = '%!v:lua.require("config.statusline").StatusLine()' end,
 })
 
 -- Redraw statusline on DiagnosticChanged
-aucmd("DiagnosticChanged", {
-  group = augroup("Status_Diagnostics"),
-  callback = vim.schedule_wrap(function()
-    vim.cmd("redrawstatus")
-  end),
+aucmd('DiagnosticChanged', {
+  group = augroup('Status_Diagnostics'),
+  callback = vim.schedule_wrap(function() vim.cmd('redrawstatus') end),
+})
+
+-- Reload file on external changes
+aucmd('FileChangedShellPost', {
+  group = augroup('FileReload'),
+  pattern = '*',
+  callback = function()
+    vim.schedule(function() vim.cmd('e') end)
+  end,
 })
 
 -- Lazy load commands
 aucmd({ 'BufReadPost', 'BufNewFile' }, {
   group = augroup('LazyLoadCommands'),
   once = true,
-  callback = function()
-    require('config.commands')
-  end,
+  callback = function() require('config.commands') end,
 })
 
 -- Lazy load LSP
 aucmd({ 'BufReadPre', 'BufNewFile' }, {
   group = augroup('LazyLoadLSP'),
   once = true,
-  callback = function()
-    require('lsp')
-  end,
+  callback = function() require('lsp') end,
 })
